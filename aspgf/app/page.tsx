196a2ec3d86f77b5f11dd07c8d14b6f311026084
AlifShaikh-786 <alifshaikh786000@gmail.com>
2026-03-06 16:52:36 +0530

    Update page.tsx

--- aspgf/app/page.tsx
+++ aspgf/app/page.tsx
@@ -1,9 +1,29 @@
 import HeroSection from "@/Components/HeroSection";
+import FAQSection from "@/Components/FAQSection";
+
+import FocusArea from "@/Components/FocusArea";
+import StatsSection from "@/Components/StatsSection";
+
+import FoundersSectionMain from "@/Components/FoundersSectionMain";
+import AboutSection from "@/Components/AboutSection";
+import WhatWeDoSection from "@/Components/WhatWeDoSection";
+// import FoundersSectionMain from "@/Components/FoundersSectionMain";
+
+
+// import ContactUs from "@/app/contactUs/ContactUs";StatsSection 
 
 export default function Page() {
   return (
     <>
       <HeroSection />
+      <AboutSection/>
+      <FocusArea/>
+    
+      <WhatWeDoSection/>
+        <StatsSection/>
+      <FoundersSectionMain/>
+      <FAQSection/>
+
     </>
   );
 }
